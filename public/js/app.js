0919d4be2c74363a5a73f3730c8b3bbf04b4d1e1
gyamps1327 <kgyampokumi@gmail.com>
2021-12-13 18:53:24 +0000

    Remove unncessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file is running");
-
 fetch("http://puzzle.mead.io/puzzle").then((response) => {
   response.json().then((data) => {
     console.log("data", data);
